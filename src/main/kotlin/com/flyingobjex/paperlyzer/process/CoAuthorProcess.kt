c76f1be7ab367159e688c9d95e7b128ed6f3b313
Steve Hendricks <steve@flyingobjex.com>
2021-11-07 15:50:11 +0000

    wip process optimization

--- src/main/kotlin/com/flyingobjex/paperlyzer/process/CoAuthorProcess.kt
+++ src/main/kotlin/com/flyingobjex/paperlyzer/process/CoAuthorProcess.kt
@@ -61,9 +61,9 @@ class CoAuthorProcess(val mongo: Mongo) : IProcess {
         log.info("\n\nCoAuthorProcess.runProcess() fetch unprocessed ::  time = $time \n\n")
 
 
-            unprocessed.parallelStream().forEach { author ->
+        unprocessed.parallelStream().forEach { author ->
 
-        val processTime = measureTimeMillis {
+            val processTime = measureTimeMillis {
                 val associatedPapers = wosRepo.getPapers(author.papers?.map { it.doi } ?: emptyList())
                 val totalPapers = associatedPapers.size
                 val totalAllAuthors = associatedPapers.sumOf { it.totalAuthors }
@@ -72,9 +72,8 @@ class CoAuthorProcess(val mongo: Mongo) : IProcess {
 
                 authorRepo.updateAuthor(author.copy(totalPapers = totalPapers, averageCoAuthors = averageCoAuthors))
             }
-            log.info("\n\nCoAuthorProcess.runProcess() parallell processTime = $processTime \n\n" )
+            log.info("CoAuthorProcess.runProcess() parallell processTime = $processTime ")
         }
-
 
 
     }
